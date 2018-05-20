7feaf3d8cb335fe9bddeea90038826b45ba49c2c
SuvorovNM <35630837+SuvorovNM@users.noreply.github.com>
2018-05-20 11:59:04 +0500

    Added comments

--- Task2_100418/Program.cs
+++ Task2_100418/Program.cs
@@ -16,15 +16,16 @@ namespace Task2_100418
     {
         static void Main(string[] args)
         {
-            int rooms=0, corridors=0;
+            int rooms=0, corridors=0;//Кол-во комнат и коридоров
             StreamReader str = new StreamReader("INPUT.TXT");
-            //Int32.TryParse(str.Read(),out rooms);
             string strin;
+            //Считывание из первой строки кол-ва комнат и коридоров
             strin = str.ReadLine();
             string[] mass = strin.Split(' ');
             Int32.TryParse(mass[0],out rooms);
             Int32.TryParse(mass[1], out corridors);
-            List<First>[] save = new List<First>[rooms+1];
+            List<First>[] save = new List<First>[rooms+1];//Лист массивов
+            //Инициализация элементов List<First>[] save
             for (int i = 0; i <= rooms; i++)
             {
                 save[i] = new List<First>();
@@ -33,27 +34,30 @@ namespace Task2_100418
             {
                 mass = str.ReadLine().Split(' ');
                 int k;
-                Int32.TryParse(mass[0], out k);
+                Int32.TryParse(mass[0], out k);//Номер первой комнаты
                 First temp = new First();
-                Int32.TryParse(mass[1], out temp.target);
-                Int32.TryParse(mass[2], out temp.colour);
-                save[k].Add(temp);
+                Int32.TryParse(mass[1], out temp.target);//Номер второй комнаты
+                Int32.TryParse(mass[2], out temp.colour);//Номер коридора
+                save[k].Add(temp);//List[номер первой комнаты].Add(Номер второй комнаты, цвет коридора)
                 First temp1 = new First();
                 int buf = temp.target;
                 temp1.target = k;
                 temp1.colour = temp.colour;
-                save[buf].Add(temp1);
+                save[buf].Add(temp1);//List[номер второй комнаты].Add(Номер первой комнаты, цвет коридора)
             }
             strin = str.ReadLine();
-            int way = Int32.Parse(strin);
-            int curpos = 1;
-            bool OK = true;
-            strin = str.ReadLine();
+            int way = Int32.Parse(strin);//Длина описания пути
+            int curpos = 1;//Текущая комната (позиция)
+            bool OK = true;//Корректность пути
+            strin = str.ReadLine();//Считывание пути из последней строки файла
             mass = strin.Split(' ');
             for (int i = 1; i <= way && OK == true; i++)
             {
-                int cor = Int32.Parse(mass[i - 1]);
+                int cor = Int32.Parse(mass[i - 1]);//Номер коридора, по которому нужно идти
                 OK = false;
+                //Поиск и проверка на существования коридора из комнаты с номером curpos
+                //При нахождении коридора curpos меняется на номер соотв. комнаты, в которую ведет коридор
+                //Если коридора заданного цвета из комнаты с номером curpos нет, но происходит выход из цикла - путь некорректен
                 for (int j = 0; j < save[curpos].Count&&OK==false; j++)
                 {
                     if (save[curpos][j].colour == cor)
